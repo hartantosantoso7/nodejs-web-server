1f1007c06bc068a30407cff864ccf24a1a8afbc4
Hartanto Santoso <hartantosantoso7@gmail.com>
2021-05-09 14:18:42 +0700

    response status

--- server.js
+++ server.js
@@ -4,18 +4,19 @@ const requestListener = (request, response) => {
     response.setHeader('content-Type', 'text/html');
 
 
-    response.statusCode = 200;
-
     const { method, url } = request;
 
     if (url === '/') {
         if (method === 'GET') {
+            response.statusCode = 200;
             response.end('<h1>Ini adalah halaman homepage</h1>');
         } else {
+            response.statusCode = 400;
             response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
         }
     } else if (url === '/about') {
         if (method === 'GET') {
+            response.statusCode = 200;
             response.end('<h1>Halo! Ini adalah halaman about</h1>')
         } else if (method === 'POST') {
             let body = [];
@@ -27,12 +28,15 @@ const requestListener = (request, response) => {
             request.on('end', () => {
                 body = Buffer.concat(body).toString();
                 const { name } = JSON.parse(body);
+                response.statusCode = 200;
                 response.end(`<h1>Hai ${name}! ini adalah halaman about</h1>`);
             });
         } else {
+            response.statusCode = 400;
             response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
         }
     } else {
+        response.statusCode = 400;
         response.end('<h1>Halaman tidak ditemukan</h1>');
     }
 
