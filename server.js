d83a18cd13905edb5dd8a1322e59d5ed68fd0cb4
Hartanto Santoso <hartantosantoso7@gmail.com>
2021-05-09 14:09:34 +0700

    Routing Request

--- server.js
+++ server.js
@@ -5,27 +5,54 @@ const requestListener = (request, response) => {
 
 
     response.statusCode = 200;
-    // response.end('<h1>Halo HTTP Server!</h1>');
 
-    const { method } = request;
+    const { method, url } = request;
 
-    if (method === 'GET') {
-        response.end('<h1>Hello</h1>');
+    if (url === '/') {
+        if (method === 'GET') {
+            response.end('<h1>Ini adalah halaman homepage</h1>');
+        } else {
+            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+        }
+    } else if (url === '/about') {
+        if (method === 'GET') {
+            response.end('<h1>Halo! Ini adalah halaman about</h1>')
+        } else if (method === 'POST') {
+            let body = [];
+
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const { name } = JSON.parse(body);
+                response.end(`<h1>Hai ${name}! ini adalah halaman about</h1>`);
+            });
+        } else {
+            response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
+        }
+    } else {
+        response.end('<h1>Halaman tidak ditemukan</h1>');
     }
+
+    // if (method === 'GET') {
+    //     response.end('<h1>Hello</h1>');
+    // }
     
-    if (method === 'POST') {
-        let body = [];
+    // if (method === 'POST') {
+    //     let body = [];
 
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
+    //     request.on('data', (chunk) => {
+    //         body.push(chunk);
+    //     });
 
-        request.on('end', () => {
-            body = Buffer.concat(body).toString();
-            response.end(`<h1>Hai ${body}</h1>`);
-        });
+    //     request.on('end', () => {
+    //         body = Buffer.concat(body).toString();
+    //         response.end(`<h1>Hai ${body}</h1>`);
+    //     });
         
-    }
+    // }
 };
 
 const server = http.createServer(requestListener);
